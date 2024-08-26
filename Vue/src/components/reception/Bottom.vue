dd67aea6e4b6ba82f9f5606eaf89f3c2b2bf6575
theoneasd <77137363+theoneasd@users.noreply.github.com>
2024-08-26 10:41:43 +0800

    second commit

---
+++ Vue/src/components/reception/Bottom.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="blog-bottom-content">
+    <div class="box">
+      <p class="Copyright">Copyright © <span>{{year}} Designed by shenjg</span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogBottom',
+  computed: {
+    year() {
+      return new Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<style lang='scss' scope>
+.blog-bottom-content {
+  position: relative;
+  bottom: 0;
+  width: 100%;
+  height: 6.25rem;
+  background-color: #596275;
+  color: #ecf0f1;
+  text-align: center;
+  .box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    a {
+      text-decoration: none;
+      color: #bdc3c7;
+    }
+  }
+}
+</style>
---
+++ Vue/src/components/system/Aside.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="aside-content">
+    <el-menu background-color="#576574" text-color="#fff" active-text-color="#54a0ff" :default-active="myPath" class="el-menu-vertical-demo" router unique-opened>
+      <!-- <el-menu-item route="/home/view" index="/home/view">
+        <i class="el-icon-s-platform" style="font-size: 24px;"></i>
+        <span>视图</span>
+      </el-menu-item> -->
+      <el-menu-item route="/home/user" index="/home/user">
+        <i class="el-icon-user" style="font-size: 24px;"></i>
+        <span>用户管理</span>
+      </el-menu-item>
+      <el-menu-item route="/welcome" index="/">
+        <i class="el-icon-position" style="font-size: 24px;"></i>
+        <span>前往前台</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Aside',
+  data() {
+    return {
+      myPath: ''
+    }
+  },
+  watch: {
+    $route: function () {
+      this.myPath = this.$route.matched[1] ? this.$route.matched[1].path : this.$route.matched[0].path
+    }
+  },
+  created() {
+    this.myPath = this.$route.matched[1] ? this.$route.matched[1].path : this.$route.matched[0].path
+  }
+}
+</script>
+
+<style lang='scss' scope>
+.aside-content {
+  width: 200px;
+  height: 100%;
+  background-color: #576574;
+  color: #333;
+  text-align: center;
+  .el-menu {
+    border: 0;
+  }
+}
+</style>
